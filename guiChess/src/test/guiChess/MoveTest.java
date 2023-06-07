021c7a7e7d2af4b48cf588ede4c511bcb586c005
qrq1356 <qrq1356@autuni.ac.nz>
2023-06-08 02:45:48 +1200

    first JUnit framework test class.
    Signed-off-by: qrq1356 <qrq1356@autuni.ac.nz>

---
+++ guiChess/src/test/guiChess/MoveTest.java
@@ -0,0 +1,16 @@
+package guiChess;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoveTest {
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+}
